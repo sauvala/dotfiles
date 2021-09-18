b9a4d2390bcb3168fe8e4bb781eeb2d983c6974f
Janne Sauvala <janne.sauvala@gmail.com>
2021-09-18 13:06:12 +0300

    Change exec-path-from-shell config phase

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -27,7 +27,7 @@
 (server-start)
 
 (use-package exec-path-from-shell
-  :con***REMOVED***g
+  :init
   (when (memq window-system '(mac ns x))
     (exec-path-from-shell-initialize)))
 
@@ -881,3 +881,26 @@ folder, otherwise delete a word"
 (use-package speed-type)
 
 (use-package bug-hunter)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init ***REMOVED***le should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(safe-local-variable-values '((org-duration-format . h:mm))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init ***REMOVED***le should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(mode-line ((t (:family "JetBrains Mono" :height 125))))
+ '(mode-line-inactive ((t (:family "JetBrains Mono" :height 125))))
+ '(org-document-title ((t (:weight bold :height 1.3))))
+ '(org-level-1 ((t (:inherit 'outline-1 :weight medium :height 1.2))))
+ '(org-level-2 ((t (:inherit 'outline-2 :weight medium :height 1.1))))
+ '(org-level-3 ((t (:inherit 'outline-3 :weight medium :height 1.05))))
+ '(org-level-4 ((t (:inherit 'outline-4 :weight medium :height 1.0))))
+ '(org-level-5 ((t (:inherit 'outline-5 :weight medium :height 1.1))))
+ '(org-level-6 ((t (:inherit 'outline-6 :weight medium :height 1.1))))
+ '(org-level-7 ((t (:inherit 'outline-7 :weight medium :height 1.1))))
+ '(org-level-8 ((t (:inherit 'outline-8 :weight medium :height 1.1))))
+ '(vertico-current ((t (:background "#3c3836")))))
