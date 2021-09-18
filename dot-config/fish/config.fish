f0550600be4c32188ba75057bf798cccc857b159
Janne Sauvala <janne.sauvala@gmail.com>
2021-09-18 14:17:27 +0300

    Enable vim keybindings in Fish

--- dot-config/fish/config.fish
+++ dot-config/fish/config.fish
@@ -38,3 +38,13 @@ base16-gruvbox-dark-medium
 
 # The next line updates PATH for the Google Cloud SDK.
 if [ -f '/Users/janne.sauvala/Downloads/google-cloud-sdk 2/path.***REMOVED***sh.inc' ]; . '/Users/janne.sauvala/Downloads/google-cloud-sdk 2/path.***REMOVED***sh.inc'; end
+
+# vim keybindings
+***REMOVED***sh_vi_key_bindings
+
+function ***REMOVED***sh_user_key_bindings
+  bind yy ***REMOVED***sh_clipboard_copy
+  bind y ***REMOVED***sh_clipboard_copy
+  bind Y ***REMOVED***sh_clipboard_copy
+  bind p ***REMOVED***sh_clipboard_paste
+end
