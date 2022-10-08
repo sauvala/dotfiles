82fbc4b05a5b8c80c1b90aed0437c927614cf786
Janne Sauvala <janne.sauvala@gmail.com>
2022-10-08 16:19:43 +0300

    Add detached

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1075,6 +1075,19 @@ folder, otherwise delete a word"
 
 (use-package denote)
 
+(use-package detached
+  :init
+  (detached-init)
+  :bind (;; Replace `async-shell-command' with `detached-shell-command'
+         ([remap async-shell-command] . detached-shell-command)
+         ;; Replace `compile' with `detached-compile'
+         ([remap compile] . detached-compile)
+         ([remap recompile] . detached-compile-recompile)
+         ;; Replace built in completion of sessions with `consult'
+         ([remap detached-open-session] . detached-consult-session))
+  :custom ((detached-show-output-on-attach t)
+           (detached-terminal-data-command system-type)))
+
 ;; (use-package all-the-icons-dired
 ;;   :hook (dired-mode . all-the-icons-dired-mode))
 
