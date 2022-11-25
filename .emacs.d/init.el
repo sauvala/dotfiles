3cccd9454f511d5a69dbf2285386a2bc96644011
Janne Sauvala <janne.sauvala@gmail.com>
2022-11-25 23:01:54 +0200

    Add base configs and version control

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -28,6 +28,13 @@
 (elpaca (elpaca :host github :repo "progfolio/elpaca"))
 (elpaca use-package (require 'use-package))
 
+(add-to-list 'load-path (expand-***REMOVED***le-name "modules" user-emacs-directory))
+
+(setq custom-***REMOVED***le (locate-user-emacs-***REMOVED***le "custom.el"))
+(require 'version-control)
+
+(when (***REMOVED***le-exists-p custom-***REMOVED***le)
+  (load custom-***REMOVED***le))
 
 ;; Make GC pauses faster by decreasing the threshold.
 (setq gc-cons-threshold (* 2 1000 1000))
---
+++ .emacs.d/modules/version-control.el
@@ -0,0 +1,5 @@
+;;;; version-control.el --- Autocompile emacs-lisp code  -*- lexical-binding: t; -*-
+
+(elpaca-use-package magit)
+
+(provide 'version-control)
