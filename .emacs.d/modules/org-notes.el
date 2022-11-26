56c57572ac1942bd3ccb42b9550f786ce5de2196
Janne Sauvala <janne.sauvala@gmail.com>
2022-11-27 00:30:33 +0200

    Fix problem loading Org

---
+++ .emacs.d/modules/org-notes.el
@@ -0,0 +1,5 @@
+;;;; org-notes.el -*- lexical-binding: t; -*-
+
+(elpaca-use-package org)
+
+(provide 'org-notes)
--- .emacs.d/modules/org.el
+++
@@ -1,5 +0,0 @@
-;;;; org.el --- Setup org and related packages -*- lexical-binding: t; -*-
-
-(elpaca-use-package org)
-
-(provide 'org)
