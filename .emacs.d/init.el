5c252ceadb2d006fe496661ae2cba7bf4275dab0
Janne Sauvala <janne.sauvala@gmail.com>
2022-11-28 23:00:10 +0200

    Add configs for MacOS

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -10,6 +10,7 @@
 (require 'init-minibuffer)
 (require 'init-vc)
 (require 'init-editor)
+(require 'init-macos)
 
 ;; Make GC pauses faster by decreasing the threshold
 (setq gc-cons-threshold (* 2 1000 1000))
