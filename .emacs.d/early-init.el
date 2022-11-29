52edc38ad2f2bb3f8639a170fa3357d4fd7416ea
Janne Sauvala <janne.sauvala@gmail.com>
2022-11-29 15:17:14 +0200

    Disable custom.el setting and loading

--- .emacs.d/early-init.el
+++ .emacs.d/early-init.el
@@ -2,18 +2,15 @@
 
 ;;; Garbage collection and perf tuning
 (setq gc-cons-threshold (* 50 1000 1000))
-(setq inhibit-compacting-font-caches t)
 (setq read-process-output-max 65536) ; (* 64 1024)
+(setq inhibit-compacting-font-caches t)
 
+;;; Emacs lisp source/compiled preference
 ;;; Prefer loading newest compiled .el ***REMOVED***le
 (customize-set-variable 'load-prefer-newer noninteractive)
 
 ;;; Packages
 (setq package-enable-at-startup nil)
-
-;;; Emacs lisp source/compiled preference
-;; Prefer loading newest compiled .el ***REMOVED***le
-(customize-set-variable 'load-prefer-newer noninteractive)
 
 ;;; UI
 (push '(tool-bar-lines . 0) default-frame-alist)
