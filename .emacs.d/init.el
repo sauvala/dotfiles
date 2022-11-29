44be849ca517a6e15402c1f8db2c871d78ec847b
Janne Sauvala <janne.sauvala@gmail.com>
2022-11-29 15:17:14 +0200

    Disable custom.el setting and loading

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,16 +1,21 @@
-;; Load custom values
-(setq custom-***REMOVED***le (locate-user-emacs-***REMOVED***le "custom.el"))
-(when (***REMOVED***le-exists-p custom-***REMOVED***le)
-  (load custom-***REMOVED***le))
+(add-to-list 'load-path (expand-***REMOVED***le-name "modules" user-emacs-directory))
 
 ;; Load modules
-(add-to-list 'load-path (expand-***REMOVED***le-name "modules" user-emacs-directory))
 (require 'init-org)
 (require 'init-completion)
 (require 'init-minibuffer)
 (require 'init-vc)
 (require 'init-editor)
 (require 'init-macos)
+
+
+;; Try if disabling setting and loading custom.el helps
+
+;;(setq custom-***REMOVED***le (locate-user-emacs-***REMOVED***le "custom.el"))
+
+;; Load custom values
+;;(when (***REMOVED***le-exists-p custom-***REMOVED***le)
+;;  (load custom-***REMOVED***le))
 
 ;; Make GC pauses faster by decreasing the threshold
 (setq gc-cons-threshold (* 2 1000 1000))
--- .emacs.d/modules/init-macos.el
+++ .emacs.d/modules/init-macos.el
@@ -1,9 +1,11 @@
 ;;;; init-macos.el -*- lexical-binding: t; -*-
 
-(when (feature 'ns)
-  (customize-set-variable mac-right-option-modi***REMOVED***er nil)
-  (customize-set-variable mac-command-modi***REMOVED***er 'super)
-  (customize-set-variable ns-function-modi***REMOVED***er 'hyper)
-  (customize-set-variable x-select-enable-clipboard t))
+;;(when (featurep 'ns)
+  (customize-set-variable 'ns-command-modi***REMOVED***er 'meta)
+  (customize-set-variable 'ns-right-option-modi***REMOVED***er nil)
+  (customize-set-variable 'ns-option-modi***REMOVED***er 'super)
+  (customize-set-variable 'ns-function-modi***REMOVED***er 'hyper)
+  ;(customize-set-variable x-select-enable-clipboard t)
+;;)
 
 (provide 'init-macos)
