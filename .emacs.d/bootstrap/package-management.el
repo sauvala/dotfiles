e2fb900948e683e1f62fac14f98b321c2f937de4
Janne Sauvala <janne.sauvala@gmail.com>
2023-03-29 18:52:04 +0300

    Replace quelpa with vc-use-package

--- .emacs.d/bootstrap/package-management.el
+++ .emacs.d/bootstrap/package-management.el
@@ -4,4 +4,8 @@
 (setq use-package-always-defer t)
 (setq use-package-always-ensure t)
 
+(unless (package-installed-p 'vc-use-package)
+  (package-vc-install "https://github.com/slotThe/vc-use-package"))
+(require 'vc-use-package)
+
 (provide 'package-management)
