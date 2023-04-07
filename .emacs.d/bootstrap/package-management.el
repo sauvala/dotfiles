ecc3cf1c73fe0779628ba361f495051c7f91facb
Janne Sauvala <janne.sauvala@gmail.com>
2023-04-08 01:40:45 +0300

    Do not report native comp warnings

--- .emacs.d/bootstrap/package-management.el
+++ .emacs.d/bootstrap/package-management.el
@@ -1,4 +1,11 @@
 ;;;; package-management.el --- Autocompile emacs-lisp code  -*- lexical-binding: t; -*-
+
+;; Disable native compilation warnings
+(use-package native-compilation
+  :ensure nil
+  :custom
+  (native-comp-async-report-warnings-errors nil))
+
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
 (setq use-package-always-defer t)
