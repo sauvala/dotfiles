022575e3b31423f5212467f2d07b79033e35bcf7
Janne Sauvala <janne.sauvala@gmail.com>
2023-08-06 23:45:31 +0300

    Add exec-path-from-shell

--- .emacs.d/modules/init-core.el
+++ .emacs.d/modules/init-core.el
@@ -7,4 +7,12 @@
   :config
   (emacs-gc-stats-mode +1))
 
+(use-package exec-path-from-shell
+  :defer 2
+  :config
+  (when (memq window-system '(mac ns x))
+    (exec-path-from-shell-initialize))
+  (when (daemonp)
+    (exec-path-from-shell-initialize)))
+
 (provide 'init-core)
